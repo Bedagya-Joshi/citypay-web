eb13db84f8746a7c69cea959e2973971d1830068
Bedagya-Joshi <5h4d0mf0rc3@gmail.com>
2023-12-19 13:13:22 +0545

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+// import 'antd/dist/antd.css';
 import './App.css';
+import { Layout, Menu, theme } from "antd";
+import './citypay_logo.png'
 
-function App() {
+//These are kept for the navbar items
+const navbarItem1 = ["Agent Signup", "Android Download", "IOS Download"];
+const navbarItemNames2 = ['Home','About', 'Services', 'Contact', 'Blog', 'Facebook', 'Instagram'];
+
+//listing and displaying the items of array in individual nav bar
+const { Header, Content, Footer } = Layout;
+const items1 = navbarItem1.map((name, index) => ({
+  key: String(index + 1),
+  label: name,
+}));
+const items2 = navbarItemNames2.map((name, index) => ({
+  key: String(index + 1),
+  label: name,
+}));
+
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="Logo Image">
+          <img src="./citypay_logo.png" alt="CityPayLogo"></img>
+        </div>
+        <div className="navbar_top" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={items1}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="navbar_bot" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          items={items2}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {/* Top section */}
+          <div>
 
+          </div>
+          {/* Section 1 */}
+          <div>
+
+          </div>
+          {/* News and Offers Section */}
+          <div>
+
+          </div>
+          {/* Section 2 */}
+          <div>
+
+          </div>
+          {/* Section 3 */}
+          <div>
+
+          </div>
+          {/* Section 4 */}
+          <div>
+
+          </div>
+          {/* Section 5 */}
+          <div>
+
+          </div>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {/* <image> */}
+      </Footer>
+    </Layout>
+  );
+};
 export default App;
+
