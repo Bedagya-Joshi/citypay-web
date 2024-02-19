4f2d1afbc51c06eca6837484e6989c28069c015a
sureshpaudel1948 <sureshpaudel59@gmail.com>
2024-02-19 15:09:46 +0545

    Contact Section, About Section and CityPay Logo Modified in the Home Section

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -6,28 +6,48 @@ const ContactPage = () => {
     <div className="contact">
       <h1>Contact Us</h1>
       <div>
-        <h2>CityPay Pvt. Ltd.</h2>
+        <h2>City Wallet Private Limited</h2><br></br>
         <div className="Office">
           <h3>Registered Office</h3>
-          <p>Kamaladi, Kathmandu, Nepal</p>
+          <p>City Express Complex, 5th Floor</p>
+          <p>Bank Road, Kamaladi, Kathmandu, Nepal</p>
+          <p>Po. Box. No. 21766</p><br></br>
         </div>
         <div className="Support">
-          <h3>Customer Support</h3>
-          <p>Toll-Free No: (For NTC only)</p>
-          <p>Toll-Free No: (For Ncell only)</p>
-          <p>Email: </p>
-          <p>Phone No: </p>
-          <p>Phone No: </p>
-          <p>Phone No: </p>
-          <p>Phone No: </p>
-          <p>Phone No: </p>
-          <p>(For general inquiry and technical assistance)</p>
+
+          <div className="cont1">
+            <h3>Contact Details</h3>
+            <p>Email: citywallet@ctxpress.com </p>
+            <p>TelePhone No: 00977-1-4531900 ext -116, 117,162, 205</p>
+            <p>Call Center: 00977-1-5970225</p>
+            <p>Toll-Free No: 16600 11 12 13</p><br></br>
+           
+            <h3>KYC Verification</h3>
+            <p> Phone No: 9844131555 (Bedagya Ji)</p>
+            <p> Phone No: 9861292429 (Sishir Ji)</p>
+            <p> Phone No: 9748429924 (Anupam Ji)</p>
+            <p> Phone No: 9840246536 (Anurag Ji)</p><br></br>
+          </div>
+
+          <div className="cont2">
+            <h3>Overall Support</h3>
+            <p> Phone No: 984-1349397 (Binay Sir)</p>
+            <p> Phone No: 985-1123927 (Prakash Sir)</p>
+            <p> Phone No: 986-0416906 (Utsav Sir)</p><br></br>
+
+            <h3>System Support</h3>
+            <p> Phone No: 9813601931 (Sunita Ji)</p>
+            <p> Phone No: 9744253687 (Shaili Ji)</p>
+            <p> Phone No: 9813937872 (Reeyaz Ji)</p>
+            <p> Phone No: 9813601931 (Nischal Ji)</p><br></br>
+          </div>
+        
         </div>
         <div className="Branch">
           <h3>Branch Offices:</h3>
           <p></p>
         </div>
-        
+
       </div>
     </div>
   );
