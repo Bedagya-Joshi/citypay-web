3810bfed5ebd7ec3e737e05d56d260f7b3c1a24c
AnuragGirii <girianurag76@gmail.com>
2024-02-23 11:52:06 +0545

    about page changes

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import ScrollToTop from "../Components/ScrollToTop";
 import "./aboutPage.css";
 
-import what from "../Assets/wallet.png";
+import cityxp from "../Assets/cityexpress.png";
 import bankvector from "../Assets/request.jpg";
 
 const AboutPage = () => {
@@ -104,17 +104,23 @@ const AboutPage = () => {
       </div>
       
       */}
-        <div className="promotors">
-          <h1>Our Promoters:</h1>
-          <p>
-            CityPAY is promoted by City Express Money Transfer Pvt. Ltd, one of
-            the largest remittance service providers in Nepal. With over 17
-            years of experience in the field of remittance management, City
-            Express Money Transfer Pvt. Ltd is one of the leading remittance
-            providers with a huge customer base and agent network across
-            different parts of Nepal.
-          </p>
-        </div>
+        <section class="promo-section">
+          <div className="promotors">
+            <h1>Our Promoters:</h1>
+            <div class="cityxp-logo">
+              <img src={cityxp} />
+              <p>
+              CityPAY is promoted by City Express Money Transfer Pvt. Ltd, one
+              of the largest remittance service providers in Nepal. With over 17
+              years of experience in the field of remittance management, City
+              Express Money Transfer Pvt. Ltd is one of the leading remittance
+              providers with a huge customer base and agent network across
+              different parts of Nepal.
+            </p>
+            </div>
+           
+          </div>
+        </section>
       </div>
     </div>
   );
